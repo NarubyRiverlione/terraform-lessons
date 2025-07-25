2fc927d21c162676de99afe29cf19fd6aad122a0
Naruby Riverlione <naruby.riverlione@gmail.com>
2025-07-25 11:36:19 +0200

    formatted

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "The environment for which the resources are being created (e.g., dev, prod)"
   type        = string
-  default     = "prod"  #  your actual environment name
+  default     = "prod" #  your actual environment name
 }
 
 variable "created_by" {
